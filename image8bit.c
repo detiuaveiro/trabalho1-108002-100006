b5e96553a24a947465c762c5858f4207d651bde9
Adriana Rocha <adrianarocha@ua.pt>
2023-11-26 21:41:16 +0000

    ``` Correção de erros ```

--- image8bit.c
+++ image8bit.c
@@ -373,9 +373,8 @@ int ImageValidRect(Image img, int x, int y, int w, int h) { ///
 // This internal function is used in ImageGetPixel / ImageSetPixel. 
 // The returned index must satisfy (0 <= index < img->width*img->height)
 static inline int G(Image img, int x, int y) {
-  int index;
   // Insert your code here!
-  double index = x + y * img->width;
+  int index = x + y * img->width;
   assert (0 <= index && index < img->width*img->height);
   return index;
 }
